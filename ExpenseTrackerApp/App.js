1c3157f43766b16ebecf7975cf3075c2698d98a5
Yunus Emre <www.yunuscinar4141@gmail.com>
2024-08-29 09:54:19 +0300

    expense tracker app done without user input

---
+++ ExpenseTrackerApp/App.js
@@ -0,0 +1,91 @@
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'expo-status-bar';
+import IconButton from './components/UI/IconButton';
+import { GlobalStyles } from './constants/styles';
+import AllExpenses from './screens/AllExpenses';
+import ManageExpense from './screens/ManageExpense';
+import RecentExpenses from './screens/RecentExpenses';
+import ExpensesContextProvider from './store/expenses-context';
+ 
+const Stack = createNativeStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+    <BottomTabs.Navigator 
+      screenOptions={({ navigation }) => ({
+        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        headerTintColor: 'white',
+        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+        tabBarActiveTintColor: GlobalStyles.colors.accent500,
+        headerRight: ({ tintColor }) => (
+          <IconButton 
+            icon="add" 
+            size={24} 
+            color={tintColor} 
+            onPress={() => {
+              navigation.navigate('ManageExpense');
+            }} 
+          />
+        ),
+      })}
+    >
+      <BottomTabs.Screen 
+        name='RecentExpenses' 
+        component={RecentExpenses} 
+        options={{
+          title: 'Recent Expenses',
+          tabBarLabel: 'Recent',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="hourglass" size={size} color={color} />
+          ), 
+        }}
+      />
+      <BottomTabs.Screen 
+        name='AllExpenses' 
+        component={AllExpenses}
+        options={{
+          title: 'All Expenses',
+          tabBarLabel: 'All Expenses',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="calendar" size={size} color={color} />
+          ),
+        }}
+      />
+    </BottomTabs.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <ExpensesContextProvider>
+        <NavigationContainer>
+          <Stack.Navigator 
+            screenOptions={{
+              headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
+              headerTintColor: 'white'
+            }}
+          >
+            <Stack.Screen 
+              name="ExpensesOverview" 
+              component={ExpensesOverview} 
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen   
+              name="ManageExpense" 
+              component={ManageExpense} 
+              options={{
+                presentation: 'modal', // it effects how screen opens 
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ExpensesContextProvider>
+    </>
+  );
+}
